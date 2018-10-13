5ee12701270d38b0cb5a0ba827d2005f696d0972
adt <aarondtaveras@gmail.com>
2018-10-12 23:31:06 -0400

    Basic parsing - line by line font changing and styling.

--- animate_folder/text-animator/src/App.js
+++ animate_folder/text-animator/src/App.js
@@ -3,14 +3,103 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      text: "It was the hunter's first time outside Montana. He woke, stricken still with the hours-old vision of ascending through rose-lit cumulus, of houses and barns like specks deep in the snowed-in valleys, all the scrolling country below looking December—brown and black hills streaked with snow, flashes of iced-over lakes, the long braids of a river gleaming at the bottom of a canyon. Above the wing the sky had deepened to a blue so pure he knew it would bring tears to his eyes if he looked long enough.",
+      lines: [],
+      fonts: [
+        "fantasy","Ubuntu","courier"
+      ],
+      colors: [
+          "red","blue","yellow"
+        ]
+      // colors:
+    };
+  }
+
+  componentDidMount(){
+    this.sliceText();
+  }
+
+  generateFont(){
+    let index = Math.floor(Math.random()*this.state.fonts.length);
+    // Math.floor(Math.random()*upper_bound);
+    console.log(this.state.fonts[index]);
+    return this.state.fonts[index];
+  }
+
+  generateColor(){
+    let index = Math.floor(Math.random()*this.state.colors.length);
+    return this.state.colors[index];
+  }
+
+  sliceText(){
+    var new_lines = [];
+    let line = "";
+    var count = 0;
+    const text_body = this.state.text;
+    const words = text_body.split(" ");
+    for(let i=0;i<words.length;i++){
+      if(count!==5)
+      {
+        line += (words[i]+" ");
+        count ++;
+      } 
+      else if(count===5)
+      {
+        if(!this.isPunctuation(words[i].charAt(words[i].length-1))){
+          line += (words[i]+"...");
+        } else {
+          line += (words[i]);
+        }
+        new_lines.push(line);
+        count = 0;
+        line = "";
+      }
+    }
+    // if there's anything left
+    if(count > 0){
+      line = "";
+      while(count){
+      line += (words[words.length - count] + " ");
+      count--;
+      }
+      new_lines.push(line);
+    }
+    
+    // set our state to our new array of lines
+    this.setState({
+      lines: new_lines
+    })
+  }
+
+  isPunctuation(char){
+    // console.log(char);
+    return (char==="," || char==="." || char==="!" || char==="!");  
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div>
+            {this.state.lines.map((line,index)=> {
+              let Color = this.generateColor();
+              let Font = this.generateFont();
+              console.log(Color);
+              console.log(Font);
+              return(
+                <p key={index} style={{color:Color,fontFamily:Font}}>
+                  {line}
+                </p>
+              );
+            })
+            }
+            
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
