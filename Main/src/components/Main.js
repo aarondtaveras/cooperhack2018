7f30485730d8037fcb8575e3ef52d46fc1c9e2a8
Toby <tobyau9000@gmail.com>
2018-10-13 05:42:07 -0400

    fixing merge error

--- Main/src/components/Main.js
+++ Main/src/components/Main.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+  constructor(){
+    this.state() = {
+      current_theme : "default";
+      themes: [
+        { name: "default", primary: "", }
+        { name: "dark", primary: "", }
+          //secondary: ""
+        }
+      ]
+    };
+    this.switchTheme = this.switchTheme.bind(this);
+  };
+
+  switchTheme(event){
+    if(this.state.current_theme==="default"){
+      this.setState({
+        current_theme: "dark"
+      })
+    }
+    else{
+      this.setState({
+        current_theme: "default"
+      })
+    }
+  }
+
   render() {
+
+    var current_color;
+    this.state.current_theme
+
     return (
       <div className="MainBox">
         <h1>Machine Learning!</h1>
